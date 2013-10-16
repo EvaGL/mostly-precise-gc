3357c45868144b6bdce52e008518067b84b0cf8a
Mariia <mari.kren@gmail.com>
2013-10-16 16:45:13 +0400

    new comments

--- Roots2/gc_ptr.h
+++ Roots2/gc_ptr.h
@@ -1,28 +1,45 @@
+/****************************************************************************************
+        * File: gc_ptr.h
+        * Description: This file describe creating gc_ptr - our pointer
+		* Last modification: 16/10/13
+*****************************************************************************************/
 #pragma once
 #include "collect.h"
 #include <vector>
 
-extern std::vector<void *> offsets;
-extern bool new_active;
+extern std::vector<void *> offsets; /**< a stored ptr on heap*/
+extern bool new_active; /**< global flag, tells where this ptr. False -- stack, true -- heap*/
 
+/**
+* @class template class gc_ptr
+* @brief the class describes our pointer gc_ptr
+* @detailed template class gc_ptr has two consructors with args and without args
+* @param kind of ptr we need to create(ptr on - what type)
+*/
 template <class T> 
 class gc_ptr {
 public:
-	T* ptr;
-	ptr_list *me;
-	bool stack_ptr;
+	T* ptr; /**< ptr of specified type*/
+	ptr_list *me; /**< list of ptrs in this obj*/
+	bool stack_ptr; /**< is this ptr on stack*/
 
+	/*	\fn construct gc_ptr()
+		\brief setting ptr on null			
+	*/
 	gc_ptr() {
 		if (!new_active) {
-			me = inc(this), stack_ptr = true;
+			me = inc(this), stack_ptr = true; /**< add current addr in ptr_list*/
 		} else {
 			stack_ptr = 0;
-			offsets.push_back(this);
+			offsets.push_back(this); /**< add our ptr in offsets list*/
 			me = 0;
 		}
 		ptr = 0;
 	}
 
+	/*	\fn construct gc_ptr(int* p)
+		\brief setting  pointer ptr on p pointer type of T 			
+	*/
 	gc_ptr(T* p) {
 		if (!new_active) {
 			me = inc(this), stack_ptr = true;
@@ -31,9 +48,12 @@ public:
 			offsets.push_back(this);
 			me = 0;
 		}
-		ptr = p;
+		ptr = p; 
 	}
 
+	/*	\fn construct gc_ptr(const gc_ptr<int> &p)
+		\brief setting ptr on given adress type of T 			
+	*/
 	gc_ptr(const gc_ptr <T> &p) {
 		if (!new_active) {
 			me = inc(this), stack_ptr = true;
@@ -44,13 +64,16 @@ public:
 		}
 		ptr = p.ptr;
 	}
-
+	/*	\fn destructor gc_ptr()
+		\brief delete current gc_ptr from special ptr_list  			
+	*/
 	~gc_ptr() {
 		if (stack_ptr) {
 			dec(me);
 		}
 	}
 
+	/*<reloaded operators for gc_ptrs objects*/
 	T& operator*() const { return *ptr; }
 	T* operator->() const {	return ptr; }
 	T* get() const { return ptr; }
