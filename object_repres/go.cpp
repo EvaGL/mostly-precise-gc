fd76fafdae209b41a8da41ff99d5dab04c91a69f
Mariia <freak-07.92@mail.ru>
2013-10-17 08:15:44 +0400

    Update go.cpp

--- object_repres/go.cpp
+++ object_repres/go.cpp
@@ -11,56 +11,56 @@
 #include <cstdlib>
 #include <vector>
 
-extern std::vector <void *> ptr_in_heap; /*< list of ptrs in heap*/
+extern std::vector <void *> ptr_in_heap; /* list of pointers in heap*/
 
-extern ptr_list* all_ptr; /*< all created ptrs*/
+extern ptr_list* all_ptr; /* all created pointers*/
 
-inline base_meta* get_meta_inf (void *v) {  /*< get the block with meta_inf*/
+inline base_meta* get_meta_inf (void *v) {  /* get the block with meta_inf*/
 	return (reinterpret_cast <base_meta*> (*(reinterpret_cast <size_t*> (reinterpret_cast <size_t>(v) - sizeof(base_meta*)))));
 }
 
-inline void* get_next_obj(void *v) {  /*< get next object*/
+inline void* get_next_obj(void *v) {  /* get next object*/
 	return reinterpret_cast <void*> (*((size_t *)v));
 }
 
-void go (void *v, bool mark_bit) {  /*< walk and mark throught the objects which we can get by walking from roots */
+void go (void *v, bool mark_bit) {  /* walk and mark throught the objects which we can get by walking from roots */
 	try {
 		if (v == 0) {
 			return;
 		}
 
 		base_meta* bm = get_meta_inf(v);
-		void *shell = bm->shell;  /*< in shell we saving tag info*/
+		void *shell = bm->shell;  /* in shell we saving tag info*/
 		BLOCK_TAG* tag = (BLOCK_TAG *) shell;
 
-		if (tag->model == 0) {  /*< checking tag modell, correct if rang 1,2,4*/
+		if (tag->model == 0) {  /* checking tag modell, correct if rang 1,2,4*/
 			throw tag;
 		}
-		if (bm->mbit == mark_bit) {  /*< if it was marked/unmarked before - done*/
+		if (bm->mbit == mark_bit) {  /* if it was marked/unmarked before - done*/
 			return;
 		}
 
-		bm->mbit = mark_bit;  /*< change if it was not*/
-		shell = shell + sizeof(BLOCK_TAG); /*< getting shell - place whith info about offsets(num and offsets)*/	
+		bm->mbit = mark_bit;  /* change if it was not*/
+		shell = shell + sizeof(BLOCK_TAG); /* getting shell - place whith info about offsets(num and offsets)*/	
 
 		switch (tag->model) {
-			case 1: {  /*< if type of model == 1, it is complex object - struct*/
+			case 1: {  /* if type of model == 1, it is complex object - struct*/
 					for (size_t j = 0; j < bm->size; j++) {  
-						void *offsets = shell;  /*< getting starting address*/
-						size_t n = *((size_t *)offsets);  /*< count of offsets*/
-						offsets += sizeof(size_t);  /*< get first offset*/
-						for (size_t i = 0; i < n; i++) {  /*< walk throught offsets*/
-							void *p = v + (*((POINTER_DESCR *)offsets)).offset;  /*< get object by offset*/
-							go(get_next_obj(p), mark_bit);  /*< mark, if we need it*/
-							offsets += sizeof(POINTER_DESCR);   /*< get next*/
+						void *offsets = shell;  /* getting starting address*/
+						size_t n = *((size_t *)offsets);  /* count of offsets*/
+						offsets += sizeof(size_t);  /* get first offset*/
+						for (size_t i = 0; i < n; i++) {  /* walk throught offsets*/
+							void *p = v + (*((POINTER_DESCR *)offsets)).offset;  /* get object by offset*/
+							go(get_next_obj(p), mark_bit);  /* mark, if we need it*/
+							offsets += sizeof(POINTER_DESCR);   /* get next*/
 						}
-						v = v + tag->size;  /*< next*/
+						v = v + tag->size;  /* next*/
 					}
 				}
 				break;
-			case 2:  /*< simple obj*/
+			case 2:  /* simple obj*/
 				break;
-			case 4:  /*< unboxed_array*/
+			case 4:  /* unboxed_array*/
 				break;
 			default:
 				throw tag;	
@@ -81,22 +81,22 @@ void mark_and_sweep () {
 	mi = mallinfo();
 	printf ("total allocated space before m&s %i\n", mi.uordblks);
 
-	for (ptr_list* root = all_ptr; root != 0; root = root->next) {  /*< walk through all ptrs in stack*/
-		go (get_next_obj(root->ptr), 1);  /*< mark all roots with mbit = 1*/ 
+	for (ptr_list* root = all_ptr; root != 0; root = root->next) {  /* walk through all pointers in stack*/
+		go (get_next_obj(root->ptr), 1);  /* mark all roots with mbit = 1*/ 
 	}
 
-	std::vector <void*> to_erase;  /*< garbage list*/
-	std::vector <void*> new_ptr_in_heap;  /*< live objects list*/
+	std::vector <void*> to_erase;  /* garbage list*/
+	std::vector <void*> new_ptr_in_heap;  /* live objects list*/
 
-	for (size_t i = 0; i < ptr_in_heap.size(); i++) {  /*< walk in heap*/	
-		base_meta* m_inf = get_meta_inf(ptr_in_heap[i]) ;  /*< get inf about mbit of object*/
+	for (size_t i = 0; i < ptr_in_heap.size(); i++) {  /* walk in heap*/	
+		base_meta* m_inf = get_meta_inf(ptr_in_heap[i]) ;  /* get inf about mbit of object*/
 		if (m_inf->mbit == 1) {
-			new_ptr_in_heap.push_back(ptr_in_heap[i]);  /*< if alive - live*/
+			new_ptr_in_heap.push_back(ptr_in_heap[i]);  /* if alive - live*/
 		} else { 
 			to_erase.push_back(ptr_in_heap[i]); /* if dead - be killed*/
 		}
 	}
-	for (size_t i = 0; i < to_erase.size(); i++) {  /*< clean all connected with dead object information*/
+	for (size_t i = 0; i < to_erase.size(); i++) {  /* clean all connected with dead object information*/
 		base_meta* m_inf = get_meta_inf(to_erase[i]);
 		m_inf->del_ptr();
 		free (m_inf->get_begin());
@@ -105,7 +105,7 @@ void mark_and_sweep () {
 	ptr_in_heap.clear();  
 	ptr_in_heap = new_ptr_in_heap;
 
-	for (size_t i = 0; i < ptr_in_heap.size(); i++) {  /*< umark all alive objects in heap*/
+	for (size_t i = 0; i < ptr_in_heap.size(); i++) {  /* umark all alive objects in heap*/
 		base_meta* m_inf = get_meta_inf(ptr_in_heap[i]);
 		m_inf->mbit = 0;
 	}
