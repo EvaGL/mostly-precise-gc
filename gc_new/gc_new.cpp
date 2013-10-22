62ba515e67ea9f4f9f2e4dea6c1f6a3237e00393
Mariia <freak-07.92@mail.ru>
2013-10-22 10:58:47 +0400

    Update gc_new.cpp

--- gc_new/gc_new.cpp
+++ gc_new/gc_new.cpp
@@ -1,12 +1,18 @@
+/*************************************************************************************//**
+        * File: gc_new.cpp
+        * Description: This file consists describing code with realiasation functions  in "gc_new.h"
+        * Update: 20/10/13
+*****************************************************************************************/
+
 #include <map>
 #include "gc_new.h"
 #include "../object_repres/go.h"
 #include <cstdio>
 #include <string>
 
-pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
-std::vector <void *> offsets;
-std::vector <void *> ptr_in_heap;
-std::unordered_map <std::string, void *> list_meta_obj;
-bool new_active = false;
-size_t counter = 0;
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;  /* create mutex for locking function of allocating */
+std::vector <void *> offsets;  /* struct offsets - stored offsets */ 
+std::vector <void *> ptr_in_heap;  /* all ptrs in heap */
+std::unordered_map <std::string, void *> list_meta_obj;  /* map objects metainfo */
+bool new_active = false;  /* where we creating object(true - heap) */
+size_t counter = 0; /* stored count of occupated blocks */
