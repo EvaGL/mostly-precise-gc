7a1723a36cbb4beb04336c97cff17815ff32a8a4
Mariia <freak-07.92@mail.ru>
2013-10-22 18:12:24 +0400

    Update gc_new.h

--- gc_new/gc_new.h
+++ gc_new/gc_new.h
@@ -31,10 +31,10 @@ using std::make_pair;
 class base_meta
 {
         public:
-        void *shell; /**< pointer on the box(meta info struct for storing offsets) of object*/
-        size_t size; /**< size of object*/
-        bool mbit; /**< mark bit(garbage or alive) of object*/        
-        virtual void del_ptr () = 0; /**< delete meta-ptr*/
+        void *shell; /**< pointer on the box(meta info struct for storing offsets) of object */
+        size_t size; /**< size of object */
+        bool mbit; /**< mark bit(garbage(set False) or alive(set True)) of object */        
+        virtual void del_ptr () = 0; /**< delete meta-ptr */
         virtual void* get_begin () = 0; /**< get begin of object(pointer on meta)*/
 };
 
@@ -68,59 +68,63 @@ template <class T> class meta : public base_meta
 /**
 * @class gc_new
 * @brief the class for allocating space and setting metainf
+* @detailed for different kinds of call different allocating 
 */
 template <class T> T* gc_new (size_t count=1)  
 {
-        while (pthread_mutex_trylock(&mut)!=0){};  /* trying to lock*/
+        while (pthread_mutex_trylock(&mut)!=0){};  /* trying to lock */
 
         counter += sizeof(T);  /* num of space that we used ++ */
-        if (counter >= 50000000) /* if occupated place more than 500000000 lets start to collect*/
+        if (counter >= 50000000) /* if occupated place more than 500000000 lets start to collect */
         {
                 mark_and_sweep();
                 counter = 0;
         }
 
-        void *res; /* create uninitialized object which will be store the result*/
+        void *res; /* ninitialize object which will be store the result */
         if (count <= 0)
                 return 0;
 
-        new_active = true;  /* set flag that create object in heap */
+        new_active = true;  /* set flag that object creates(allocates) in heap */
         offsets.clear();  /* clean from old offsets for new object */
-
-        if (count == 1) {  /* case pointer on one-word type*/
-                res = malloc(sizeof(T) + sizeof(void*) + sizeof(meta<T>));  /* allocate space*/
-                new (res + sizeof(void*) + sizeof(meta<T>)) T;  /* call constructor of heap => call constructor of gc_ptr=> get new struct offsets*/
+        
+       /*<allocating space and creating meta data for pointers
+       *case: count == 1 --- pointer on one-word type
+       *otherwise: other pointers(...)*/
+        if (count == 1) {
+                res = malloc(sizeof(T) + sizeof(void*) + sizeof(meta<T>));  /* allocate space */
+                new (res + sizeof(void*) + sizeof(meta<T>)) T;  /* call constructor of heap => call constructor of gc_ptr=> get new struct offsets */
                 *((size_t*)(res + sizeof(meta<T>))) =  reinterpret_cast <size_t> (new (res) meta<T>);  /* initialize meta in obj */
-                meta<T>* m_inf = reinterpret_cast <meta<T>* > (res);  /* stored pointer on meta*/
+                meta<T>* m_inf = reinterpret_cast <meta<T>* > (res);  /* stored pointer on meta */
 
                 if (offsets.size() == 0) {  /* if num offsets inside object is empty */
-                        if (list_meta_obj.count(typeid(T).name()))  /* if type of this  meta stored in list_meta_obj*/
-                                m_inf->shell = list_meta_obj[typeid(T).name()];  /* just save it in current shell*/
-                        else { /*if doesn't*/
-                                m_inf->shell = generic_box_simple (); /* create new type - box, save in shell*/
-                                list_meta_obj[typeid(T).name()] = m_inf->shell; /* add in list with the same types*/
+                        if (list_meta_obj.count(typeid(T).name()))  /* if type of this  meta stored in list_meta_obj */
+                                m_inf->shell = list_meta_obj[typeid(T).name()];  /* just save it in current shell */
+                        else { /*if doesn't */
+                                m_inf->shell = generic_box_simple (); /* create new type - box, save in shell */
+                                list_meta_obj[typeid(T).name()] = m_inf->shell; /* add in list with the same types */
                         }
                 }
-                else { /*case offset count more than 1, doing the same things except created boxes*/
+                else { /*case offset count more than 1, doing the same things except created boxes */
                         std::list <size_t> offsets_ptr;  
-                        for (size_t i = 0; i < offsets.size(); i++) {  /* getting all offsets_ptrs*/
-                                offsets_ptr.push_front(reinterpret_cast <size_t> (offsets[i]) - reinterpret_cast <size_t> (res + sizeof(void*) + sizeof(meta<T>)));  // getting ptrs
+                        for (size_t i = 0; i < offsets.size(); i++) {  /* getting all offsets_ptrs */
+                                offsets_ptr.push_front(reinterpret_cast <size_t> (offsets[i]) - reinterpret_cast <size_t> (res + sizeof(void*) + sizeof(meta<T>)));  /* getting pointers */
                         }
                         if (list_meta_obj.count(typeid(T).name()))  
                                 m_inf->shell = list_meta_obj[typeid(T).name()]; 
                         else 
                         {
-                                m_inf->shell = generic_box_struct (offsets_ptr, sizeof(T), count); /*create new box and save pointer in shell*/
+                                m_inf->shell = generic_box_struct (offsets_ptr, sizeof(T), count); /*create new box and save pointer in shell */
                                 list_meta_obj[typeid(T).name()] = m_inf->shell;  
                         }
                 }
-                m_inf->size = count;  /* count of offsets*/
+                m_inf->size = count;  /* count of offsets */
                 m_inf->mbit = 0;
-                m_inf->ptr = (T*)(res + sizeof(base_meta*) + sizeof(meta<T>)); /*set pointer on value of object(gc_ptr)*/
+                m_inf->ptr = (T*)(res + sizeof(base_meta*) + sizeof(meta<T>)); /*set pointer on value of object(gc_ptr) */
         }
-        else {  /* if gc_new <name_of_type> (2..n) - size of sruct more than 1 word, the same actions except allocating space*/
+        else { 
                 res = malloc(sizeof(T) * count + sizeof(void *) + sizeof(meta<T>));
-                new (res + sizeof(void*) + sizeof(meta<T>)) T[count]; /* here we calling constructor from T[count]*/
+                new (res + sizeof(void*) + sizeof(meta<T>)) T[count]; /* here we calling constructor from T[count] */
                 *((size_t*)(res + sizeof(meta<T>))) = reinterpret_cast <size_t> (new (res) meta<T>);
                 meta<T>* m_inf = reinterpret_cast <meta<T>* > (res);                
 
@@ -152,11 +156,11 @@ template <class T> T* gc_new (size_t count=1)
                 m_inf->ptr = (T*)(res + sizeof(base_meta*) + sizeof(meta<T>));
         }
 
-        ptr_in_heap.push_back(res + sizeof(base_meta*) + sizeof(meta<T>));  /* adding ptr in list of heap pointers*/
+        ptr_in_heap.push_back(res + sizeof(base_meta*) + sizeof(meta<T>));  /* adding ptr in list of heap pointers */
         new_active = false;  
-        offsets.clear();  /* finished - cleaning*/
-        pthread_mutex_unlock(&mut);  /* unlocking*/
+        offsets.clear();  /* finished - cleaning */
+        pthread_mutex_unlock(&mut);  /* unlocking */
 
-        return (T*)(res + sizeof(base_meta*) + sizeof(meta<T>));  /*return ptr on allocated space, begining value*/
+        return (T*)(res + sizeof(base_meta*) + sizeof(meta<T>));  /*return ptr on allocated space, begining value */
 
 }
