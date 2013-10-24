0fb8d1767101c7728adc9066e7eaf66934ed96b2
Mariia <freak-07.92@mail.ru>
2013-10-24 17:31:53 +0400

    Added new comments

--- gc_new/gc_new.cpp
+++ gc_new/gc_new.cpp
@@ -11,8 +11,8 @@
 #include <string>
 
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;  /* create mutex for locking function of allocating */
-std::vector <void *> offsets;  /* struct offsets - stored offsets */ 
-std::vector <void *> ptr_in_heap;  /* all ptrs in heap */
+std::vector<void *> offsets; /* a stored pointers addreses in heap when new_active flag == true else its clear*/
+bool new_active = false; /* global flag. False -- out gc_new, true -- in gc_new*/
+std::vector <void *> ptr_in_heap;  /* all pointers in heap */
 std::unordered_map <std::string, void *> list_meta_obj;  /* map objects metainfo */
-bool new_active = false;  /* where we creating object(true - heap) */
 size_t counter = 0; /* stored count of occupated blocks */
