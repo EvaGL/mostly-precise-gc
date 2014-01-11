def935b3311f20c71c6dee0abb2c52f18cbdeac2
danyaberezun <danya.berezun@gmail.com>
2014-01-11 17:36:50 +0400

    delete unused includes from libgc.h

--- libgc.h
+++ libgc.h
@@ -1,18 +1,3 @@
 #pragma once
-#include "sources/boxing2.h"
-#include "sources/collect.h"
 #include "sources/gc_new.h"
 #include "sources/gc_ptr.h"
-#include "sources/go.h"
-#include "sources/stack.h"
-#include "sources/taginfo.h"
-#include "sources/PointerList.h"
-#include "dlmalloc/mark.h"
-
-#include <cstdio>
-#include <pthread.h>
-#include <vector>
-#include <typeinfo>
-#include <unordered_map>
-#include <string>
-#include <cstdio>
