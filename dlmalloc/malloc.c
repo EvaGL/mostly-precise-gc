efa36615efc33e07d8b8422dd8dbcd766e27d18e
danyaberezun <danya.berezun@gmail.com>
2014-01-15 14:54:19 +0400

    ref

--- dlmalloc/malloc.c
+++ dlmalloc/malloc.c
@@ -6305,9 +6305,9 @@ DLMALLOC_EXPORT size_t sweep() {
       mchunkptr q = align_as_chunk(s->base);
       while (segment_holds(s, q) &&
              q < m->top && q->head != FENCEPOST_HEAD) {
-        // printf("chunk: %p\n", q);
+        printf("chunk: %p\n", q);
         if (!flag4inuse(q) && is_inuse(q)) {
-            // free(chunk2mem(q));
+            free(chunk2mem(q));
             // printf("free that chunk\n");
         }
         if (q < m->top && q->head != FENCEPOST_HEAD) {
