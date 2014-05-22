39168b9c5fe4c7fa37397c4c54ab1d74ac603010
danyaberezun <danya.berezun@gmail.com>
2014-05-22 17:39:25 +0400

    add boxed array mark

--- sources/go.cpp
+++ sources/go.cpp
@@ -10,13 +10,7 @@
 #include "PointerList.h"
 #include "fake_roots.h"
 
-extern "C" {
-	void mark (void *);
-	size_t get_mark (void *);
-	void sweep (void);
-	void printDlMallocInfo (void);
-	int is_heap_pointer (void *);
-}
+// #define DEBUGE_MODE
 
 extern StackMap stack_ptr;
 extern PointerList * offsets;
@@ -26,93 +20,79 @@ inline void* get_next_obj(void *v) {  /* get the next object*/
 }
 
 inline base_meta* get_meta_inf (void *v) {  /*!< get the block with meta_inf*/
-	if (DEBUGE_MODE) {
-		printf("in get_meta_inf %p ", v);
-		fflush(stdout);
-	}
 	base_meta* res = (reinterpret_cast <base_meta*> (*(reinterpret_cast <size_t*> (reinterpret_cast <size_t>(v) - sizeof(base_meta*)))));
-	if (DEBUGE_MODE) {
-		printf("end\n");
-		fflush(stdout);
-	}
 	return res;
 }
 
 void go (void * v) {
-	if (DEBUGE_MODE) {
-		printf("\n\tin go ");
-		fflush(stdout);
-	}
-
 	try {
 		if (v == NULL || !is_heap_pointer(v)) {
+			#ifdef DEBUGE_MODE
+				printf(" %p !is_heap_pointer \n ", v);
+			#endif
 			return;
 		}
-		
-		if (DEBUGE_MODE) {
-			printf(" 1 ");
+		#ifdef DEBUGE_MODE
+			printf("\nin go\n");
 			fflush(stdout);
-		}
+		#endif
+		
 		base_meta* bm = get_meta_inf(v); /* get metainformation from object*/
 
 		if (get_mark(bm) != 0 || get_mark(v) != 0) { /* if marked --- return*/
-			if (DEBUGE_MODE) {
+			#ifdef DEBUGE_MODE
 				printf(" already marked \n ");
 				fflush(stdout);
-			}
+			#endif
 			return;
 		}
-		if (DEBUGE_MODE) {
-			printf(" 2 ");
-			fflush(stdout);
-		}
 		void *shell = bm->shell;  /* saving ponter on meta object in shell */
-		if (DEBUGE_MODE) {
-			printf(" 3 ");
-			fflush(stdout);
-		}
 		BLOCK_TAG* tag = (BLOCK_TAG *) shell; /* store shell in tag */
-		if (DEBUGE_MODE) {
-			printf(" 4: %p ", tag);
-			fflush(stdout);
-		}
 		if (tag->model == 0) {  /* checking tag modell, correct if rang 1,2,4*/
-			if (DEBUGE_MODE) {
-				printf(" 4.1 ");
-				fflush(stdout);
-			}
 			throw tag;
-		}
-		if (DEBUGE_MODE) {
-			printf(" 5 ");
-			fflush(stdout);
 		}
 
 		mark(bm);
-		shell = (char *)shell + sizeof(BLOCK_TAG); /* get next part shell - offsets(num and offsets)*/	
+		#ifdef DEBUGE_MODE
+			printf("tag->model:%i \n", tag->model);
+			fflush(stdout);
+		#endif
 
 		switch (tag->model) {
-			case 1: {  /* if type of model == 1, it is complex object - struct*/
-					if (DEBUGE_MODE) {
-						printf(" tag model 1 ");
-						fflush(stdout);
-					}
-					for (size_t j = 0; j < bm->size; j++) { 
-						void * this_offsets = shell;  /* get address of the offsets begin*/
-						size_t n = *((size_t *)this_offsets);  /* count of offsets*/
-						this_offsets = (char *)this_offsets + sizeof(size_t);  /* get first offset*/
-						for (size_t i = 0; i < n; i++) {  /* walk throught offsets*/
-							void *p = (char*)v + (*((POINTER_DESCR *)this_offsets)).offset;  /* get object by offset*/
-							if (p) {
-								go(get_next_obj(p));  /* go deeper and mark*/
-							}
-							this_offsets = (char *)this_offsets + sizeof(POINTER_DESCR);   /* get next pointer in this obj*/
+			case 1: {  /* boxed object */
+					size_t n = *(size_t *)((char *)shell + sizeof(BLOCK_TAG));  /* count of offsets*/
+					void * this_offsets = (char *)shell + sizeof(BLOCK_TAG) + sizeof(size_t);  /* get first offset*/
+					for (size_t i = 0; i < n; i++) {  /* walk throught offsets*/
+						void *p = (char*)v + (*((POINTER_DESCR *)this_offsets)).offset;  /* get object by offset*/
+						if (p) {
+							go(get_next_obj(p));  /* go deeper and mark*/
 						}
-						v = (char *)v + tag->size;  /* get next object */
+						this_offsets = (char *)this_offsets + sizeof(POINTER_DESCR);   /* get next pointer in this obj*/
 					}
+					v = (char *)v + tag->size;  /* get next object */
 				}
 				break;
 			case 2:  /* simple obj*/
+				break;
+			case 3: {  /* boxed array*/
+					void * poin = v;
+					size_t sizeType = tag->num_of_el, sizeArray = tag->size;
+					for (size_t i = 0; i < sizeArray; i++, poin = (void *)((char *)poin + sizeType)) {
+						#ifdef DEBUGE_MODE
+							printf(" %i ", i);
+						#endif
+						void * meta = tag->ptr;
+						void * this_offsets = (char *)meta + sizeof(BLOCK_TAG) + sizeof(size_t);
+						size_t n = *(size_t *)((char *)meta + sizeof(BLOCK_TAG));
+						for (size_t i = 0; i < n; i++) {
+							void *p = (char*)poin + (*((POINTER_DESCR *)this_offsets)).offset;
+							if (p) {
+								go(get_next_obj(p));
+							}
+							this_offsets = (char *)this_offsets + sizeof(POINTER_DESCR);
+						}
+					}
+				}
 				break;
 			case 4:  /* unboxed_array*/
 				break;
@@ -134,26 +114,10 @@ void go (void * v) {
 
 void mark_and_sweep () {
 	printf("before m&s "); printDlMallocInfo();
-	if (DEBUGE_MODE) {
-		printf("in mark_and_sweep\n");
-		fflush(stdout);
-	}
 	mark_fake_roots();
 	for(Iterator root = stack_ptr.begin(); root <= stack_ptr.end(); root++) {/* walk through all roots*/
-		if (DEBUGE_MODE) {
-			printf(" root %p; ", *root);
-			fflush(stdout);
-		}
 		go (get_next_obj(*root)); /* mark all available objects with mbit = 1*/
 	}
-	if (DEBUGE_MODE) {
-		printf("\nsweep\n");
-		fflush(stdout);
-	}
 	sweep();
-	if (DEBUGE_MODE) {
-		printf("end sweep; end mark_and_sweep\n");
-		fflush(stdout);
-	}
 	printf("after m&s "); printDlMallocInfo();
 }
