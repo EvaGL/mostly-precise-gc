f3f383c375a96486a45b02b2b61428016a044af2
danyaberezun <danya.berezun@gmail.com>
2014-06-05 13:56:11 +0400

    fix bug && adds asserts

--- sources/stack.cpp
+++ sources/stack.cpp
@@ -10,16 +10,17 @@ StackMap * StackMap::instance = NULL;
 StackMap::StackMap(size_t length1,
  		int free_page_parameter1,
  		int add_page_parameter1
-		):  length(length1) {
+ 		) {
 	if (DEBUG_MODE) {
 		printf("StackMap::StackMap(size_t length1,...\n");
 		fflush(stdout);
 	}
 	StackMap::instance = this;
 	stackElement_size = sizeof(struct StackElement);
-	page_size = length / stackElement_size;
+	page_size = length1 / stackElement_size;
+	length = page_size * stackElement_size;
 	if (free_page_parameter1 == -1)
-		free_page_parameter = page_size * 1.5;
+		free_page_parameter = page_size * 3;
 	else
 		free_page_parameter = free_page_parameter1;
 	if (add_page_parameter1 == -1)
@@ -57,18 +58,28 @@ void StackMap::register_stack_root(void * newAddr) {
 		printf("void StackMap::register_stack_root(void* newAddr) {\n");
 		fflush(stdout);
 	}
-	if (top + add_page_parameter == end_of_mapped_space) {
+	if (top + add_page_parameter > end_of_mapped_space) {
 		// mmap one more memory page
-		end_of_mapped_space = (struct StackElement *) mmap(end_of_mapped_space, length, PROT_WRITE | PROT_READ, MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-		assert(end_of_mapped_space != MAP_FAILED);
+		StackElement * temp = (struct StackElement *) mmap(end_of_mapped_space, length, PROT_WRITE | PROT_READ, MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		assert(temp != MAP_FAILED);
+		assert(temp == end_of_mapped_space);
 		if (DEBUG_MODE) {
 			printf("StackMap: allocate one more memory page: ... %p -- new end ... ", (end_of_mapped_space + page_size));
 			fflush(stdout);
 		}
-		end_of_mapped_space += page_size;
+		end_of_mapped_space = temp + page_size;
 	} 
+	if (DEBUG_MODE) {
+		printf("top - map_begin = %li \n", (long)top - (long)map_begin);
+		fflush(stdout);
+	}
 	top ++;
 	top->addr = newAddr;
+	if (DEBUG_MODE) {
+		printf("top - map_begin = %li \n", (long)top - (long)map_begin);
+		fflush(stdout);
+	}
+	assert(top >= map_begin);
 	if (DEBUG_MODE) {
 		printf("done\n");
 		fflush(stdout);
@@ -81,11 +92,12 @@ void StackMap::delete_stack_root() {
 		fflush(stdout);
 	}
 	top --;
+	assert(top >= map_begin);
 	if (top + free_page_parameter < end_of_mapped_space)
 	{
 		if (munmap(end_of_mapped_space, length) == 0) {
 			end_of_mapped_space -= page_size;
-			assert(end_of_mapped_space > map_begin);
+			assert(end_of_mapped_space >= map_begin);
 		} else if (DEBUG_MODE) {
 			printf("StackMap: WARNING: memory page cannot be free ?!? \n");
 			fflush(stdout);
