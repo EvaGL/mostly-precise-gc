47c474eeca816d9e80187bf9388d2c5e3bfb72e2
danyaberezun <danya.berezun@gmail.com>
2014-11-18 12:09:03 +0400

    description and TODO is added

--- sources/gc_new.cpp
+++ sources/gc_new.cpp
@@ -1,6 +1,7 @@
 /*************************************************************************************//**
         * File: gc_new.cpp
-        * Description: This file consists describing code with realiasation functions  in "gc_new.h"
+        * Description: This file consists some global data.
+        TODO: global data might be disposed of ...
 *****************************************************************************************/
 
 #include "gc_new.h"
@@ -8,11 +9,14 @@
 #include <cstdio>
 #include "stack.h"
 
-StackMap stack_ptr = StackMap::create_StackMap_instance(); //!< stack of stack pointers
-std::vector <size_t> offsets;
-bool new_active = false; /* global flag. False -- out gc_new, true -- in gc_new*/
-MetaInformation * list_meta_obj = new MetaInformation();
-size_t counter = 0; /* stored count of occupated blocks */
-int nesting_level = 0; /* gc_new nesting level */
-bool no_active = false; /* flag that says gc_ptr not to add himself in roots or offsets */
+StackMap stack_ptr = StackMap::create_StackMap_instance();	//!< root stack
+std::vector <size_t> offsets;	/// global data to store offsets from the class
+								/// constructs in gc_ptr;
+								/// releases and transforms to class meta in gc_new.
+bool new_active = false; /* global flag. False -- gc_new is unactive, true -- gc_new is active*/
+MetaInformation * list_meta_obj = new MetaInformation();	/// global list with class meta
+/// TODO: counter might be releazed
+size_t counter = 0;	/* stored count of occupated blocks */
+int nesting_level = 0;	/* gc_new nesting level (recursion depth) */
+bool no_active = false;	/* flag that says gc_ptr might not to add himself in roots or offsets */
 size_t current_pointer_to_object = 0;
