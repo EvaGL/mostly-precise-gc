2f95e03433343b8cc2583d31ae05f63c19cd9492
Aleksandr Samofalov <booger4096@gmail.com>
2015-04-26 01:18:02 +0300

    Fix test. I hate C++

--- tests/binary_tree_sort/binary_tree_sort.cpp
+++ tests/binary_tree_sort/binary_tree_sort.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <libprecisegc/libprecisegc.h>
 #include "List.h"
-#include "Tree.h"
 
+// You may need to change it to fix segfault in threading.cpp:117
+static const int STACK_OVERFLOW_FACTOR = 500;
 static int array_size = 2;
 bool debug_print = true;
 
@@ -14,7 +15,7 @@ void test () {
 	}
 
 	gc_ptr<List> list = gc_new<List, gc_ptr<int>, int>(mas, array_size);
-	while (rand() % 50 != 1) {
+	while (rand() % 50 != 1 && list->count() < STACK_OVERFLOW_FACTOR) {
 		dprintf ("1\n"); fflush (stdout);
 		list->insert(mas, array_size);
 		dprintf ("2\n"); fflush (stdout);
@@ -38,6 +39,9 @@ int main (int argc, char * argv[]) {
 	// 		debug_print = (arg.compare(8, 1, "1") == 0);
 	// 	}
 	// }
+	int seed = time(0);
+	printf("seed = %d\n", seed);
+	srand(seed);
 	 for (int i = 0; i < 300; i++) {
 		test();
 	 }
